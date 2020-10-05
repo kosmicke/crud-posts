e026fc515ff9816a141f55e8c56d04c3e75601a8
Andre Rodrigues <andre.cs.rodrigues@gmail.com>
2020-10-05 20:52:46 -0300

    created pages

--- posts-app/src/App.js
+++ posts-app/src/App.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import './App.css';
 
-class App extends React.Component{
+class App extends React.Component {
 
-  render(){
+  render() {
     return (
-      <div className="container">
-        <h1>App</h1>
-      </div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="/">Post App</a>
+        <button className="navbar-toggler" 
+          type="button" 
+          data-toggle="collapse" 
+          data-target="#navbarMenu" 
+          aria-controls="navbarMenu">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarMenu">
+          <div className="navbar-nav">
+            <a href="/" className="nav-item nav-link">Home</a>
+            <a href="/" className="nav-item nav-link">Posts</a>
+          </div>
+        </div>
+      </nav>
     );
-  } 
-
+  }
+  
 }
 
 export default App;
