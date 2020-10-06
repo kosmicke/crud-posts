058f5a46a8e1f39f3bc7c8005b6b50b7f8913fe4
Andre Rodrigues <andre.cs.rodrigues@gmail.com>
2020-10-05 22:46:19 -0300

    routes and links

---
+++ posts-app/src/pages/post-list/post-list.page.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class PostListPage extends React.Component {
+
+    render() {
+        return (
+            <div className="container">
+                <h1>PostListPage</h1>
+            </div>
+        )
+    }
+
+}
+
+export default PostListPage;
