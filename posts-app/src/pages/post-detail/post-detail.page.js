8ce73da5d3ca2fea54d738bd3e527deaaabf5a3c
Andre Rodrigues <andre.cs.rodrigues@gmail.com>
2020-10-05 22:46:19 -0300

    routes and links

---
+++ posts-app/src/pages/post-detail/post-detail.page.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class PostDetailPage extends React.Component{
+
+    render(){
+        return(
+            <div className="container">
+                <h1>PostDetailPage</h1>
+            </div>
+        )
+    }
+    
+}
+
+export default PostDetailPage;
