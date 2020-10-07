b6299f94d747b17030c24ce326c5a797adea0fdf
André Rodrigues <andre.urbanico@gmail.com>
2020-10-06 21:31:09 -0300

    finisehd login

--- posts-app/src/App.js
+++ posts-app/src/App.js
@@ -7,8 +7,32 @@ import LoginPage from './pages/login/login.page';
 import PostListPage from './pages/post-list/post-list.page';
 import PostDetailPage from './pages/post-detail/post-detail.page';
 import PostEditPage from './pages/post-edit/post-edit.page';
+import authService from './services/auth.service';
 
 class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      userData : null
+    }
+  }
+
+  componentDidMount(){
+    this.loadUserData()
+  }
+  
+  loadUserData(){
+    let userData = authService.getLoggedUser()
+    if(userData){
+      this.setState({ userData : userData })
+    }
+  }
+
+  logout(){
+    authService.clearLoggedUser();
+    window.location.reload();
+  }
 
   render() {
     return (
@@ -27,11 +51,20 @@ class App extends React.Component {
               <Link to="/" className="nav-item nav-link">Home</Link>
               <Link to="/post-list" className="nav-item nav-link">Posts</Link>
             </div>
+            {(this.state.userData) ? (
+                <div className="nav-user">
+                  <div className="nav-user__info">
+                    <h4>{this.state.userData.name}</h4>
+                    <p>{this.state.userData.email}</p>
+                  </div>
+                  <button className="btn btn-outline-dark" onClick={e => this.logout()}>Sair</button>
+                </div>
+              ) : null}
           </div>
         </nav>
         <Switch>
           <Route path="/" exact={true} component={HomePage} />
-          <Route path="/login" component={LoginPage} />
+          <Route path="/login" component={props => <LoginPage {...props} onLogin={() => this.loadUserData()}/>}/>
           <Route path="/post-list" component={PostListPage} />
           <Route path="/post-detail/:id" component={PostDetailPage} />
           <Route path="/post-add" component={PostEditPage} />
